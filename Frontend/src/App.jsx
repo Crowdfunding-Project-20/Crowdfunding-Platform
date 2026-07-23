4e59f2a081282ac7a146c79450f942f1cb1809b8
Anna-jay950 <gamadiannie@gmail.com>
2026-07-23 23:49:17 +0000

    bootstrapping the frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        HIIII MY NAME IS ANNIE, AND I AN BROKE!
+      </h1>
+    </>
+  )
+}
+
+export default App
