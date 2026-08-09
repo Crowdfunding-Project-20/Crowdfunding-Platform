e8e4e9ec87f26ffd32cef9520bbbf0e53e80811b
Anna-jay950 <gamadiannie@gmail.com>
2026-08-09 13:40:53 +0000

    docker

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-        HIIII MY NAME IS ANNIE, AND I AN BROKE!
+        HIIII MY NAME IS ANNIE, AND I AM BROKE!
       </h1>
     </>
   )
